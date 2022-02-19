1b0bef8611d208a070b1762849941b9abe5a008f
KiranAnnabominaEpita <kiran.annabomina@epita.fr>
2022-02-19 05:24:31 +0100

    main: Console API's in Javascript.

---
+++ index.js
@@ -0,0 +1,11 @@
+// 1. Ways to print in Javascript
+// document.write("This is document write");
+// console.log("Hello World");
+// alert("I'm alert");
+
+// 2. JavaScript console API (Application Programming Interface)
+console.log("Hello World", 4 + 6, "Another log", "Kiran");
+console.warn("This is a warning");
+console.error("This is an error");
+
+// Variables in Javascript
